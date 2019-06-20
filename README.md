7b0579002db4d7e5ecaf25c761a53e62f0a1c7cc
Quinton Ashley <qashto@gmail.com>
2019-06-19 21:44:05 -0400

    3.0.0

--- README.md
+++ README.md
@@ -5,15 +5,8 @@ Hide scrollbars in Firefox while still being able to scroll!
 ### Install
 
 [firefox addon](https://addons.mozilla.org/en-US/firefox/addon/hide-scrollbars/)
-[userscript](https://github.com/quinton-ashley/firefox-hide-scrollbars/raw/master/js/qashto_firefox-hide-scrollbars.user.js)
 
-### Credits
+### Please Donate!
 
-<https://gist.github.com/hensm/d3e2316c092e9df0c449abfcad24bc78>
-
-### Donate
-
-Ethereum: 0xb4355179da353f1BA4AA0BB5a7E3Ba4FdC7128ea  
-Bitcoin: 1562cPDAqTCtF1oX6RPMy1n8Rx6jvzYTvN  
 <https://www.patreon.com/qashto>  
 [paypal.me/qashto/5](paypal.me/qashto/5)
--- js/background.js
+++ js/background.js
@@ -1,31 +1,13 @@
 var scrollbarsHidden = true;
 var contentScript;
-// var exclude1 = ['*://accounts.google.com/*', '*://photos.google.com/*'];
-// exclude = exclude.concat(exclude1);
 
 function hideScrollbars() {
-	var exts = [
-		'jpg', 'png', 'gif', 'mp4', 'tiff', 'skg', 'tif', 'avi', 'mov', 'flac', 'wav', 'aif', 'm4a', 'mqa'
-	];
-	let exclude = [];
-	var i;
-	for (i = 0; i < exts.length; i++) {
-		exclude.push('*://*/*.' + exts[i]);
-		exclude.push('*://*/*.' + exts[i].toUpperCase());
-	}
-	exclude.push('*://accounts.google.com/*');
-	exclude.push('*://mail.google.com/*');
-	console.log(exclude);
 
 	browser.contentScripts.register({
-		js: [{
-			file: 'js/qashto_firefox-hide-scrollbars.user.js'
-		}],
 		css: [{
 			file: 'css/content.css'
 		}],
 		matches: ['<all_urls>'],
-		excludeMatches: exclude,
 		runAt: 'document_start'
 	}).then(contentScriptObject => {
 		contentScript = contentScriptObject;
